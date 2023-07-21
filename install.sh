535d29d9eb2ba915ab156d227fd7ccbb6950b8bd
phantomic12 <76127596+phantomic12@users.noreply.github.com>
2023-07-21 18:29:02 -0400

    update deps

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mv /home/runner/deps/circleload /usr/bin/circleload
 mv osu.py /home/runner/osu.py
 mv map.py /home/runner/map.py
-pip install osrparse requests
+pip install -r requirements.txt
 chmod +x /usr/bin/circleload
 cd /home/runner
