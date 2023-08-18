a3999f712cabbc457df91747f32a15ef848cf2da
phantomic12 <yoavtirosh54@gmail.com>
2023-08-18 11:05:36 -0400

    redo install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 mv /home/runner/deps/circleload /usr/bin/circleload
-mv osu.py /home/runner/osu.py
-mv map.py /home/runner/map.py
-mv sr.py /home/runner/sr.py
-mv up.py /home/runner/up.py
-pip install -r requirements.txt
-chmod +x /usr/bin/circleload
+mv /home/runner/deps/osu.py /home/runner/osu.py
+mv /home/runner/deps/sr.py /home/runner/sr.py
+mv /home/runner/deps/map.py /home/runner/map.py
+mv /home/runner/deps/title.sh /usr/bin/title
+pip3 install -r /home/runner/deps/requirements.txt
 cd /home/runner
+
