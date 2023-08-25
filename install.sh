e589b230f30b7bfdfb48667260368da28f71ddd5
phantomic12 <76127596+phantomic12@users.noreply.github.com>
2023-08-25 15:48:36 -0400

    pwdify

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
-mv /home/runner/deps/circleload /usr/bin/circleload
-mv /home/runner/deps/osu.py /home/runner/osu.py
-mv /home/runner/deps/sr.py /home/runner/sr.py
-mv /home/runner/deps/map.py /home/runner/map.py
-mv /home/runner/deps/mapid.py /home/runner/mapid.py
-mv /home/runner/deps/title.sh /home/runner/title.sh
-pip3 install -r /home/runner/deps/requirements.txt
-cd /home/runner
+pip3 install -r /requirements.txt
 
